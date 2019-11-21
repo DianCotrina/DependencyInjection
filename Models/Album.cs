4bfef1a2f7332002f0afae0e7420a6d61bf506c9
dcotrina <diego.cotrina@cignium.com>
2019-11-21 13:38:54 -0500

    Add project files.

---
+++ Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+        public string Tittle { get; set; }
+        public double Price { get; set; }
+
+    }
+}
